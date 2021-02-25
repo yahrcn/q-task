42874a9ce4a6c3c2296994834d4b9db77c7a4851
hrcn <hrcnjk@ya.ru>
2021-02-25 15:11:23 +0400

    Added carousel (slider). Added buttons prev, next, back to main, switch to local/remote.

--- src/js/pages/slider.js
+++ src/js/pages/slider.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { setImages } from "../redux/actions";
+import { ROUTES } from "../routes";
+
+import first from "../../img/1.jpg";
+import second from "../../img/2.jpg";
+import third from "../../img/3.jpg";
+
+const images = [first, second, third];
 
 const mapStateToProps = (store) => ({
   photos: store.photos,
@@ -13,6 +21,16 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 class Slider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      remote: false,
+      text: "remote",
+      array: images,
+      currentId: 0,
+    };
+  }
+
   componentDidMount() {
     fetch("https://imagesapi.osora.ru/", {
       method: "POST",
@@ -23,15 +41,65 @@ class Slider extends React.Component {
       });
   }
 
+  handleNext() {
+    const lastIndex = this.state.array.length - 1;
+    const resetIndex = this.state.currentId === lastIndex;
+    const index = resetIndex ? 0 : this.state.currentId + 1;
+    this.setState({
+      currentId: index,
+    });
+  }
+
+  handlePrev() {
+    const lastIndex = this.state.array.length - 1;
+    const resetIndex = this.state.currentId === 0;
+    const index = resetIndex ? lastIndex : this.state.currentId - 1;
+    this.setState({
+      currentId: index,
+    });
+  }
+
   render() {
     return (
       <div className="page">
-        <div className="text">Slider</div>
-        <div className="slider">
-          {this.props.photos.map((photo, id) => {
-            return <img src={photo} key={id} alt="Кошечка" />;
-          })}
+        <div className="sliderWrapper">
+          <div className="button" onClick={this.handlePrev.bind(this)}>
+            prev
+          </div>
+          {this.state.remote ? (
+            <img
+              className="slider"
+              alt=""
+              src={this.props.photos[this.state.currentId]}
+            />
+          ) : (
+            <img className="slider" alt="" src={images[this.state.currentId]} />
+          )}
+
+          <div className="button" onClick={this.handleNext.bind(this)}>
+            next
+          </div>
         </div>
+        <div
+          className="button button_switch"
+          onClick={() => {
+            this.setState(
+              this.state.remote
+                ? { remote: false, text: "remote", array: images, currentId: 0 }
+                : {
+                    remote: true,
+                    text: "local",
+                    array: this.props.photos,
+                    currentId: 0,
+                  }
+            );
+          }}
+        >
+          switch to {this.state.text}
+        </div>
+        <Link to={ROUTES.main.path} className="button">
+          back to main
+        </Link>
       </div>
     );
   }

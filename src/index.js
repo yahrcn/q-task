591ee050a204deb4292a72df7cff5bdb79c6f906
hrcn <hrcnjk@ya.ru>
2021-02-25 13:42:29 +0400

    Deleted history prop from Router, now using BrowserRouter as Router instead of Router. Added import reset.scss

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createBrowserHistory } from "history";
 import reportWebVitals from "./reportWebVitals";
 
 import App from "./js/App";
 
-import { store } from './js/config'
+import { store } from "./js/config";
 import "./scss/index.scss";
-
-const history = createBrowserHistory();
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
+    <Router history>
       <App />
     </Router>
   </Provider>,

06b8f3e319e75bb9ce7e79f9ca025443531dfa59
hrcn <hrcnjk@ya.ru>
2021-02-25 08:57:57 +0400

    Added redux, store contains images from api.

--- src/js/pages/slider.js
+++ src/js/pages/slider.js
@@ -1,12 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
+import { setImages } from "../redux/actions";
 
 class Slider extends React.Component {
+  componentDidMount() {
+    fetch("https://imagesapi.osora.ru/", {
+      method: "POST",
+      body: "none",
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        this.props.dispatch(setImages([result[0], result[1], result[2]]));
+      });
+  }
+
   render() {
     return (
       <div className="page">
         <div className="text">Slider</div>
-        <div className="slider">{this.props.photos}</div>
+        <div className="slider">
+          {this.props.photos.map((photo, id) => {
+            return <img src={photo} key={id} alt="Кошечка" />;
+          })}
+        </div>
       </div>
     );
   }
